16ff0b3154a17836955be16062e27b5ffe0033d2
Tiku-Kouki <k022g0079@g.neec.ac.jp>
2024-05-08 15:33:52 +0900

    02_01

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -2,8 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class PlayerScript : MonoBehaviour
 {
+    public Rigidbody rb;
+
+    
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,31 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector3 v = rb.velocity;
+
+        float moveSpeed = 1.0f;
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            v.x = moveSpeed;
+        }
+        else
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            v.x = -moveSpeed;
+        }
+        else
+        {
+            v.x = 0;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            v.y = 5;
+        }
         
+
+
+            rb.velocity = v;
     }
 }
